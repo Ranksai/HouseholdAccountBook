594e17c562b71f4778de8053f5c368f2a2dd34c2
Ranksai <ranks.j.inter1908@gmail.com>
2018-02-27 18:04:35 +0900

    fix: table

--- db/01_create_table.sql
+++ db/01_create_table.sql
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS `house_account_book` (
   `name` VARCHAR(100) NOT NULL,
   `description` TEXT,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC)),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC))
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `account_item` (
@@ -48,7 +48,7 @@ CREATE TABLE IF NOT EXISTS `account_item` (
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `account_id` SMALLINT UNSIGNED NOT NULL,
   `item_id` SMALLINT UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `account_accout_type` (
